625bdda11e75a50379083a909ee0fca09645f239
Shefil Joy <shefilrocks@gmail.com>
2021-08-27 01:32:07 +0530

    Delta Appdev Task1

--- app/src/main/java/com/example/appdevtask1/Activity2.java
+++ app/src/main/java/com/example/appdevtask1/Activity2.java
@@ -1,41 +1,89 @@
-package com.example.appdevtask1;
+ package com.example.appdevtask1;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Vibrator;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
-public class Activity2 extends AppCompatActivity {
+ public class Activity2 extends AppCompatActivity {
 
-    TextView question;
+    TextView question, timer;
     Button button1, button2, button3, button4;
     Button button;
-    LinearLayout linear1,linear2;
-    TextView text7;
-    int score=0;
-    int Answer;
-    String crctans;
+    LinearLayout linear1,linear2, linearLayout;
+    TextView text7,text10;
+    int score=0, highScore;
+    int Answer,L1,L2,LL;
+    String crctans, Qns, resultText= "",result="";
+    Vibrator vibrator;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    private long Start_Time = 30000;
+    private static final long Start_Time_bg = 1000;
+    private static final long Start_time_bgw = 1000;
+
+    private CountDownTimer tCountDownTimer;
+    private CountDownTimer bCountDownTimer;
+    private boolean tTimerRunning;
+    private boolean bTimerRunning;
+    private long tTimeLeft;
+    private long bTimeLeft = Start_Time_bg;
+    private CountDownTimer wCountDownTimer;
+    private long wTimeLeft = Start_time_bgw;
 
     private final View.OnClickListener listener= new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             if(((Button) findViewById(v.getId())).getText().toString().equals(crctans)){
+               /* resetTimer();
                 printQue();
                 score+=3;
+                vibrator.vibrate(250);
+                linear1.setBackgroundColor(Color.GREEN);
+                startTimer(); */
+                bgStartTimer();
+                linear1.setBackgroundColor(Color.GREEN);
+                vibrator.vibrate(250);
+
             }
             else {
-                linear2.setVisibility(View.INVISIBLE);
+                score-=1;
+                if (score>highScore){
+                    editor.putInt("highScore",score);
+                    editor.commit();
+                }
+                /* linear2.setVisibility(View.INVISIBLE);
                 linear1.setVisibility(View.INVISIBLE);
-                text7.setVisibility(View.VISIBLE);
-                text7.setText(MainActivity.getName() + "'s Score is " + "\n" +
-                        score);
+                linearLayout.setVisibility(View.VISIBLE);
+                text7.setText(MainActivity.getName() + "'s Score is " + "\n" + score);
+                vibrator.vibrate(500);
+                linearLayout.setBackgroundColor(Color.RED);
+                tCountDownTimer.cancel(); */
+                bgwStartTimer();
+                linear1.setBackgroundColor(Color.RED);
+                vibrator.vibrate(500);
+                text7.setText(MainActivity.getName() + "'s Score: " + score);
+                text10.setText("Number of Correct Answers: " + (score+1)/3
+                        + "\n" + "Number of Wrong Answers: 1" + "\n"
+                        + "Un-attempted Questions: 0");
 
             }
         }
@@ -55,22 +103,37 @@ public class Activity2 extends AppCompatActivity {
         button3= findViewById(R.id.button3);
         button4= findViewById(R.id.button4);
         text7= findViewById(R.id.textView7);
-        text7.setVisibility(View.INVISIBLE);
+        vibrator= (Vibrator)getSystemService(VIBRATOR_SERVICE);
+        linearLayout= findViewById(R.id.linearlayout);
+        timer= findViewById(R.id.timer);
+        text10= findViewById(R.id.textView10);
+        if (Qns == null){
+            Qns = generateRandomDate();
+        }
+        //text7.setVisibility(View.INVISIBLE);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = sharedPreferences.edit();
+        highScore= sharedPreferences.getInt("highScore",0);
+
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //startTimer();
                 linear1.setVisibility(View.VISIBLE);
                 linear2.setVisibility(View.INVISIBLE);
-            } {
-                question.setText(generateRandomDate());
+                tTimeLeft=Start_Time;
+                startTimer();
+                Qns = generateRandomDate();
+                question.setText(Qns);
                 getOptions();
                 button1.setText(options.get(0));
                 button2.setText(options.get(1));
                 button3.setText(options.get(2));
                 button4.setText(options.get(3));
                 options.clear();
+
             }
         });
         button1.setOnClickListener(listener);
@@ -95,6 +158,10 @@ public class Activity2 extends AppCompatActivity {
 
         Answer = gc.get(GregorianCalendar.DAY_OF_WEEK);
 
+        /*Qns = "Q. What Day of the Week is " + gc.get(GregorianCalendar.DAY_OF_MONTH) + "/" +
+                (gc.get(GregorianCalendar.MONTH) +1) + "/" +
+                gc.get(GregorianCalendar.YEAR);*/
+
         return "Q. What Day of the Week is " + gc.get(GregorianCalendar.DAY_OF_MONTH) + "/" +
                 (gc.get(GregorianCalendar.MONTH) +1) + "/" +
                 gc.get(GregorianCalendar.YEAR);
@@ -104,7 +171,7 @@ public class Activity2 extends AppCompatActivity {
         return start + (int) Math.round(Math.random() * (end - start));
 
     }
-     private final ArrayList<String> options=new ArrayList<>();
+     private ArrayList<String> options=new ArrayList<>();
     public void getOptions(){
         ArrayList<String> day=new ArrayList<>();
         day.add("SUNDAY");
@@ -125,7 +192,8 @@ public class Activity2 extends AppCompatActivity {
         optionList= options;
     }
     public void printQue(){
-        question.setText(generateRandomDate());
+        //Qns = generateRandomDate();
+        question.setText(Qns);
         getOptions();
         button1.setText(options.get(0));
         button2.setText(options.get(1));
@@ -136,6 +204,150 @@ public class Activity2 extends AppCompatActivity {
     @Override
     public void onBackPressed(){
         if (text7.getVisibility()==View.VISIBLE)
-            finishAffinity();
+            startActivity(new Intent(Activity2.this, MainActivity.class));
     }
-}
+    private void startTimer(){
+        tCountDownTimer = new CountDownTimer(tTimeLeft, 1) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tTimeLeft=millisUntilFinished;
+                updateCountDown();
+            }
+
+            @Override
+            public void onFinish() {
+                vibrator.vibrate(500);
+                bgwStartTimer();
+                linear1.setBackgroundColor(Color.RED);
+                text7.setText(MainActivity.getName() + "'s Score is " + score);
+                text10.setText("Number of Correct Answers: " + score/3
+                        + "\n" + "Number of Wrong Answers: 0" + "\n"
+                        + "Un-attempted Questions: 1");
+                if (score>highScore){
+                    editor.putInt("highScore",score);
+                    editor.commit();
+                }
+
+            }
+        }.start();
+        tTimerRunning = true;
+    }
+    private void resetTimer() {
+        tTimeLeft = Start_Time;
+        updateCountDown();
+        tCountDownTimer.cancel();
+    }
+
+    private void updateCountDown() {
+        int sec = (int) tTimeLeft / 1000;
+        String timeLeft = String.format("Time Left: " + sec);
+        timer.setText(timeLeft);
+    }
+
+    private void bgStartTimer(){
+        bCountDownTimer=new CountDownTimer(bTimeLeft,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                bTimeLeft = millisUntilFinished;
+            }
+
+            @Override
+            public void onFinish() {
+                linear1.setBackgroundColor(Color.TRANSPARENT);
+                resetTimer();
+                Qns= generateRandomDate();
+                printQue();
+                score+=3;
+                //vibrator.vibrate(250);
+                //linear1.setBackgroundColor(Color.GREEN);
+                startTimer();
+
+            }
+        }.start();
+    }
+    private void bgwStartTimer(){
+        wCountDownTimer = new CountDownTimer(wTimeLeft,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                wTimeLeft= millisUntilFinished;
+            }
+
+            @Override
+            public void onFinish() {
+                linear2.setVisibility(View.INVISIBLE);
+                linear1.setVisibility(View.INVISIBLE);
+                linear1.setBackgroundColor(Color.TRANSPARENT);
+                linearLayout.setVisibility(View.VISIBLE);
+                //text7.setText(MainActivity.getName() + "'s Score is " + "\n" + score);
+                //vibrator.vibrate(500);
+                //linearLayout.setBackgroundColor(Color.RED);
+                tCountDownTimer.cancel();
+
+            }
+        }.start();
+    }
+
+     @Override
+     public void onSaveInstanceState(@NonNull Bundle outState) {
+         super.onSaveInstanceState(outState);
+         outState.putInt("Score", score);
+         outState.putString("GRD", Qns);
+         outState.putInt("Ans", Answer);
+         outState.putLong("timer", tTimeLeft);
+         outState.putStringArrayList("options",optionList);
+         outState.putInt("L1",linear1.getVisibility());
+         outState.putInt("L2",linear2.getVisibility());
+         outState.putInt("LL",linearLayout.getVisibility());
+         if (text7.getVisibility()==View.VISIBLE){
+             outState.putString("Result1", text7.getText().toString());
+             outState.putString("Result",text10.getText().toString());
+         }
+
+
+     }
+
+     @Override
+     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+         super.onRestoreInstanceState(savedInstanceState);
+         score= savedInstanceState.getInt("Score",0);
+         Qns = savedInstanceState.getString("GRD");
+         Answer = savedInstanceState.getInt("Ans");
+         tTimeLeft = savedInstanceState.getLong("timer");
+         Log.v("timer","" + tTimeLeft);
+         //startTimer();
+         optionList = savedInstanceState.getStringArrayList("options");
+         printQue();
+         /*if(Qns != ""){
+             bCountDownTimer.cancel();
+             Log.v("Shefil","Shefil");
+         }*/
+         Log.v("Shefil", Qns);
+         //bCountDownTimer.cancel();
+         //startTimer();
+         //updateCountDown();
+         linear2.setVisibility(savedInstanceState.getInt("L2"));
+         linear1.setVisibility(savedInstanceState.getInt("L1"));
+         linearLayout.setVisibility(savedInstanceState.getInt("LL"));
+         if (text7.getVisibility()==View.VISIBLE){
+             text7.setText(savedInstanceState.getString("Result1"));
+             text10.setText(savedInstanceState.getString("Result"));
+         }
+         if (linear2.getVisibility()==View.VISIBLE){
+             tTimeLeft=30000;
+         }
+         if (linear1.getVisibility()==View.VISIBLE){
+             startTimer();
+         }
+
+
+
+     }
+     @Override
+     protected void onStop(){
+        super.onStop();
+        if (tCountDownTimer!=null){
+        tCountDownTimer.cancel();
+     }
+    }
+ }
+
--- app/src/main/java/com/example/appdevtask1/MainActivity.java
+++ app/src/main/java/com/example/appdevtask1/MainActivity.java
@@ -2,25 +2,33 @@ package com.example.appdevtask1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
         public Button button;
         EditText editText;
         private static String name;
+        TextView textView12;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText= findViewById(R.id.editTextTextPersonName);
         button = findViewById(R.id.startBtn);
+        textView12 = findViewById(R.id.textView12);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        int high = sharedPreferences.getInt("highScore",0);
+        textView12.setText("HighScore: " + high);
 
         editText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -54,5 +62,9 @@ public class MainActivity extends AppCompatActivity {
     }
     public static String getName(){
         return name;
+    }
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
     }
 }
